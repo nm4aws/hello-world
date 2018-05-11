1622fb8d03fa50eca8b4f7d71e2179e581acb23d
nm4aws <39013899+nm4aws@users.noreply.github.com>
2018-05-12 05:26:42 +0530

    minor wordings changed

--- code/index.js
+++ code/index.js
@@ -1,12 +1,12 @@
-//index js: simple nodejs app: basic app
+//index js: simple nodejs app: basic hello world app
 
 const port = 9000
 require('http')
   .createServer((req, res) => {
     console.log('url:', req.url)
-    res.end('hello world')
+    res.end('hello world!')
   })
   .listen(port, (error)=>{
-    console.log(`server is running on ${port}`)
+    console.log(`server is now running on ${port}`)
   })
 
