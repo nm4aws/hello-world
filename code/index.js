617351e4ec1252de0354cedc947dce652c5e6394
nm4aws <39013899+nm4aws@users.noreply.github.com>
2018-05-13 01:07:54 +0530

    response text update

--- code/index.js
+++ code/index.js
@@ -5,7 +5,7 @@ const port = 9000
 require('http')
   .createServer((req, res) => {
     console.log('url:', req.url)
-    res.end('Hello World! From Node App')
+    res.end('Hello World! From Node App. Version2')
   })
   .listen(port, (error)=>{
     console.log(`server is now running on ${port}`)
