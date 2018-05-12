d5d38cbc45c9966dddc37513424a7f1b28c74bb4
nm4aws <39013899+nm4aws@users.noreply.github.com>
2018-05-13 02:56:17 +0530

    another version change

--- code/index.js
+++ code/index.js
@@ -5,7 +5,7 @@ const port = 9000
 require('http')
   .createServer((req, res) => {
     console.log('url:', req.url)
-    res.end('Hello World! From Node App. Version5')
+    res.end('Hello World! From Node App. Version6')
   })
   .listen(port, (error)=>{
     console.log(`server is now running on ${port}`)
