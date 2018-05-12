93fafe106d84fd51ac10ddd0271352cad0626f83
nm4aws <39013899+nm4aws@users.noreply.github.com>
2018-05-13 04:08:34 +0530

    updating code as per db reader

--- code/index.js
+++ code/index.js
@@ -1,13 +1,68 @@
-//index js: simple nodejs app.........
+const express = require('express');
+const mysql = require('mysql');
+const config = require('./config');
 
-//create server, listen on port and respond with hello world
-const port = 9000
-require('http')
-  .createServer((req, res) => {
-    console.log('url:', req.url)
-    res.end('Hello World! From Node App. Version8<br>')
-  })
-  .listen(port, (error)=>{
-    console.log(`server is now running on ${port}`)
-  })
+// create express app
+const app = express();
 
+//get database connection
+var connection = mysql.createConnection({
+
+  //from config
+  host : config.host,
+  user : config.user,
+  password : config.password,
+  database : config.database
+
+});
+
+connection.connect(function(error) {
+  if (!!error){
+    console.log('error connecting to db');
+  } else {
+    console.log('successfully connected to db');
+  }
+});
+
+//Get content from DB : promise version
+//print table content when requested
+app.get('/', (req, res) => {
+
+  var promise = new Promise(function(resolve, reject) {
+    if (!connection || connection.state == 'disconnected') {
+      //error case as connection is not established
+      console.log("connection object not found");
+      resolve("connection object not found");
+    } else {
+      connection.query(config.query, function(error, results, fields) {
+        if (!!error) {
+          //error case
+          console.log(error);
+          reject(Error("error in query"));
+        } else {
+          //success case
+          resolve(results);
+        }
+      });
+    }
+  });
+  
+  promise.then(function(result) {
+    var jsonoutput = JSON.stringify(result);
+    console.log(jsonoutput); // "Stuff worked!"
+    res.status(200).send(jsonoutput);
+  }, function(err) {
+    console.log(err); // Error: "It broke"
+  });
+
+});
+
+app.get('/healthcheck', (req, res) => {
+  res.status(200).send('ok');
+});
+
+// listen for requests
+var port = 9000;
+app.listen(port, () => {
+  console.log("Server is listening on port :", port);
+});
