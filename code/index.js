1bb05e4a88ab6aa2e4a7fcdb5cc8e2ca380014b6
nm4aws <39013899+nm4aws@users.noreply.github.com>
2018-05-13 04:21:18 +0530

    updating label

--- code/index.js
+++ code/index.js
@@ -50,7 +50,7 @@ app.get('/', (req, res) => {
   promise.then(function(result) {
     var jsonoutput = JSON.stringify(result);
     console.log(jsonoutput); // "Stuff worked!"
-    res.status(200).send("DB output is: " + jsonoutput);
+    res.status(200).send("DB output in json format is: " + jsonoutput);
   }, function(err) {
     console.log(err); // Error: "It broke"
   });
